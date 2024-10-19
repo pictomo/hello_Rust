ce4f715e99f6027cfbe9e21cf8faac5b0abbd45d
pictomo <tomo.pict@gmail.com>
2024-10-19 22:46:45 +0900

    [add]checker削除, 文字列から数を抽出するlexer追加

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 use regex::Regex;
 
-fn main() {
-    let result: Result<&str, &str> = checker("-1.992");
+fn main() -> Result<(), String> {
+    let text: &str = " 32  -1.992 \n";
+    let text_invalid: &str = " 32  -1.992  3. ";
+    let result: Result<Vec<String>, &str> = lexer(text);
     match result {
-        Ok(r) => println!("{}", r),
-        Err(e) => println!("Error: {}", e),
+        Ok(tokens) => {
+            for token in tokens {
+                println!("{}", token)
+            }
+        }
+        Err(e) => {
+            return Err(format!("In lexer: {}", e.to_string()));
+        }
     }
+
+    Ok(())
 }
 
-fn checker(s: &str) -> Result<&str, &str> {
-    let num_re: Regex = Regex::new(r"^-?[0-9]+(.[0-9]*)?$").unwrap();
-
-    if num_re.is_match(s) {
-        return Ok(s);
-    } else {
-        return Err(r#"Not a number"#);
+fn lexer(s_: &str) -> Result<Vec<String>, &str> {
+    let mut s: String = s_.to_string();
+    let re_head_num: Regex = Regex::new(r"^-?[0-9]+(.[0-9]+)?").unwrap();
+    let mut tokens: Vec<String> = Vec::new();
+    loop {
+        s = s.trim().to_string();
+        if s.len() == 0 {
+            break;
+        }
+        if let Some(token) = re_head_num.find(&s) {
+            tokens.push(token.as_str().to_string());
+            s = s[token.end()..].to_string();
+        } else {
+            return Err("Unexpected pattern.");
+        }
     }
+    return Ok(tokens);
 }
