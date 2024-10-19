6896eec98707004f8a4f4ace7d7dcc4ad69937f4
pictomo <tomo.pict@gmail.com>
2024-10-19 19:11:07 +0900

    [add]文字列を識別するchecker関数

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let result: Result<&str, &str> = checker("hello");
+    match result {
+        Ok(r) => println!("{}", r),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn checker(s: &str) -> Result<&str, &str> {
+    if s == "hello" {
+        return Ok(s);
+    } else {
+        return Err(r#"Not "hello""#);
+    }
 }
